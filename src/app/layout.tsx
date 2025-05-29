736fbc96646dd2d1e157d976dc3bb14f19353655
jalal1985khan <jalal1985khan@gmail.com>
2025-05-30 00:07:50 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+<Script src="https://analytics-iota-two.vercel.app/analytics-tracker.js"/>
+
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
