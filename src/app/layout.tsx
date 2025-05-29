a60f71b829fbaf51f1bd6c2b77e393fcf6de7c50
jalal1985khan <jalal1985khan@gmail.com>
2025-05-30 00:13:43 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
